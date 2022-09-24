a5acf5a462bb24e920c5316b367b2e2f1314e738
Rizki Darmawan <80609220+rizkidarmawan21@users.noreply.github.com>
2022-09-24 15:02:17 +0700

    Update OperatorPerbandingan.php

--- PHP Dasar/OperatorPerbandingan.php
+++ PHP Dasar/OperatorPerbandingan.php
@@ -5,3 +5,6 @@ var_dump("10" === 10); // false
 
 var_dump(10 > 0);
 var_dump(10 < 9);
+
+// == sama dengan 2 untuk mengecek value aja atau isinya saja
+// ===  dia mengecek value dan tipe datanya juga
